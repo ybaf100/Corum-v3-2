602319fff3d8999421ac04ca977ab8c1301f557f
ybadfg78 <ybadfg78@gmail.com>
2026-06-08 21:33:52 +0900

    set github pages base

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  // GitHub Pages 저장소명이 달라도 동작하기 쉽게 상대 경로 사용
-  base: "./",
+  base: "/Corum-v3/",
 });
